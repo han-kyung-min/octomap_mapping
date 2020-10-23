dec1c090c19aa4a3b1ed1546e3a638bb4348ffd1
hankm <thezest76@gmail.com>
2020-10-23 11:07:37 +0900

    aabb_box

---
+++ octomap_server/demo/demo_key2point.cpp
@@ -0,0 +1,19 @@
+/*
+ * demo_key2point.cpp
+ *
+ *  Created on: Oct 20, 2020
+ *      Author: hankm
+ */
+
+#include <ros/ros.h>
+#include <octomap_server/OctomapServer.h>
+
+using namespace octomap_server;
+
+
+int main(int argc, char** argv)
+{
+
+
+
+}
