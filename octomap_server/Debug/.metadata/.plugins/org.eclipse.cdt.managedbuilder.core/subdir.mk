408ca2dd46b10cfdb4e89bfa3d2631fa26b23fc8
hankm <thezest76@gmail.com>
2021-01-23 13:50:46 +0900

    dummy

---
+++ octomap_server/Debug/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.cpp 
+
+C_SRCS += \
+../.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.c 
+
+OBJS += \
+./.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.o 
+
+CPP_DEPS += \
+./.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.d 
+
+C_DEPS += \
+./.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/%.o: ../.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I/opt/ros/noetic/include -I/home/hankm/catkin_ws/src -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/%.o: ../.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/opt/ros/noetic/include -I/home/hankm/catkin_ws/src -I/home/hankm/catkin_ws/devel/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
